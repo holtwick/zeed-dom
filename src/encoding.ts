ef75a79218715a1c835fa0d3a2f7df9dccd01852
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-02-23 12:07:18 +0100

    Anly escape simple entities

--- src/encoding.ts
+++ src/encoding.ts
@@ -1,8 +1,15 @@
-import { decode, encode } from "he"
+import { decode } from "he"
 
 export const escapeHTML = (text: string) =>
-  encode(text, {
-    useNamedReferences: true,
-  })
+  text
+    .replace(/&/g, "&amp;")
+    .replace(/</g, "&lt;")
+    .replace(/>/g, "&gt;")
+    .replace(/'/g, "&apos;")
+    .replace(/"/g, "&quot;")
+
+// encode(text, {
+//   useNamedReferences: true,
+// })
 
 export const unescapeHTML = (html: string) => decode(html)
