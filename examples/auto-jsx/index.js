a03d98ef2e2755e0e36864d2cba0e05528eeb70b
Dirk Holtwick <dirk.holtwick@gmail.com>
2022-08-26 09:54:49 +0200

    Play with jsx-runtime

---
+++ examples/auto-jsx/index.js
@@ -0,0 +1,7 @@
+"use strict";
+exports.__esModule = true;
+var dom = (<ol className="projects">
+    <li>hostic</li>
+    <li>hostic-dom</li>
+  </ol>);
+console.log(dom);
---
+++ examples/auto-jsx/index.tsx
@@ -0,0 +1,10 @@
+import {h} from 'zeed-dom/jsx-runtime'
+
+let dom = (
+  <ol className="projects">
+    <li>hostic</li>
+    <li>hostic-dom</li>
+  </ol>
+)
+
+console.log(dom)
