2312dd476d9c9b3c1a72c1ab65b4e57d55d321d9
Dirk Holtwick <dirk.holtwick@gmail.com>
2023-12-16 18:08:17 +0100

    feat: handleHTMLFile

---
+++ src/index.node.ts
@@ -0,0 +1,4 @@
+// (C)opyright 2023 Dirk Holtwick, holtwick.it. All rights reserved.
+
+export * from './index'
+export { handleHTMLFile } from './node'
---
+++ src/node.ts
@@ -0,0 +1,17 @@
+import { readFileSync, writeFileSync } from 'node:fs'
+import type { VHTMLDocument } from './vdom'
+import { parseHTML } from './vdomparser'
+
+/** Manipulate HTMl file directly on disk. Only writes back if there were significant changes. */
+export function handleHTMLFile(filePath: string, handler: (document: VHTMLDocument) => void, outPath?: string): string {
+  const html = readFileSync(filePath, 'utf-8')
+  const document = parseHTML(html)
+  const htmlIn = document.render()
+  handler(document as VHTMLDocument)
+  const htmlOut = document.render()
+  if (outPath || htmlOut !== htmlIn) {
+    writeFileSync(outPath ?? filePath, htmlOut, 'utf-8')
+    return htmlOut
+  }
+  return html
+}
