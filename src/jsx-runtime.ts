ffc998091c126f45b36c485f181bce2e5857ec8c
Dirk Holtwick <dirk.holtwick@gmail.com>
2024-05-16 15:30:15 +0200

    fix: again fixing jsx types

--- src/jsx-runtime.ts
+++ src/jsx-runtime.ts
@@ -1,7 +1,5 @@
 import { h } from './html'
 
-export * from './jsx-intrinsicelements'
-
 // See
 // https://esbuild.github.io/api/#jsx-import-source
 // https://www.typescriptlang.org/tsconfig/#jsxImportSource
